6e302d40a914278f79b9fbf76e0ac3304e9ed2fb
Evgeniy Larkov <elarkov@inbox.ru>
2021-02-12 14:13:14 +0700

    Небольшие правки по коду

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-import './data.js';
 import { createSimilarOffers } from './data.js';
 const SIMILAR_OFFER_COUNT = 10;
 
-createSimilarOffers(SIMILAR_OFFER_COUNT);
+console.log(createSimilarOffers(SIMILAR_OFFER_COUNT));
 
 
 
