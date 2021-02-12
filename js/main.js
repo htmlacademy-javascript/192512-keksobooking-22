1ad397fee55e2883c5b666dcf6ceb460e49365f4
Evgeniy Larkov <elarkov@inbox.ru>
2021-02-12 14:19:54 +0700

    Remove console.log

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { createSimilarOffers } from './data.js';
 const SIMILAR_OFFER_COUNT = 10;
 
-console.log(createSimilarOffers(SIMILAR_OFFER_COUNT));
+createSimilarOffers(SIMILAR_OFFER_COUNT);
 
 
 
