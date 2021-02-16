e94d07f0536ffc38e471423af3091dd790c96e12
Evgeniy Larkov <elarkov@inbox.ru>
2021-02-16 18:29:10 +0700

    Fix minor bugs

--- js/markup.js
+++ js/markup.js
@@ -6,10 +6,11 @@ const newCard = templateCard.querySelector('.popup');
 const mapCanvas = document.querySelector('.map__canvas');
 
 
-const similarCards = createSimilarOffers();
+const similarCards = createSimilarOffers(1);
+
 const cardFragment = document.createDocumentFragment();
 
-similarCards.forEach(offer => {
+similarCards.forEach(({ offer, author }) => {
   const cardElement = newCard.cloneNode(true);
 
   const titlePopup = cardElement.querySelector('.popup__title');
@@ -23,28 +24,25 @@ similarCards.forEach(offer => {
   const photosPopup = cardElement.querySelector('.popup__photos');
   const avatarPopup = cardElement.querySelector('.popup__avatar');
 
-  titlePopup.textContent = offer.offer.title;
-  addressPopup.textContent = offer.offer.address;
-  pricePopup.textContent = `${offer.offer.price} ₽/ночь`;
-  typePopup.textContent = offer.offer.type;
-  capacityPopup.textContent = `${offer.offer.rooms} комнаты для ${offer.offer.guests} гостей`;
-  timePopup.textContent = `Заезд после ${offer.offer.checkin}, выезд до ${offer.offer.checkout}`;
-  featuresPopup.innerHTML = `
-      <li class="popup__feature popup__feature--wifi">${ offer.offer.features }</li>
-      <li class="popup__feature popup__feature--dishwasher">${ offer.offer.features }</li>
-      <li class="popup__feature popup__feature--parking">${ offer.offer.features }</li>
-      <li class="popup__feature popup__feature--washer">${ offer.offer.features }</li>
-      <li class="popup__feature popup__feature--elevator">${ offer.offer.features }</li>
-      <li class="popup__feature popup__feature--conditioner">${ offer.offer.features }</li>
-  `;
-  descPopup.textContent = offer.offer.description;
-  photosPopup.innerHTML = `
-  <img src="${offer.offer.photos[0]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">
-  <img src="${offer.offer.photos[1]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">
-  <img src="${offer.offer.photos[2]}" class="popup__photo" width="45" height="40" alt="Фотография жилья">`;
-  avatarPopup.src = offer.author.avatar;
+  titlePopup.textContent = offer.title;
+  addressPopup.textContent = offer.address;
+  pricePopup.textContent = `${offer.price} ₽/ночь`;
+  typePopup.textContent = offer.type;
+  capacityPopup.textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  timePopup.textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  featuresPopup.innerHTML = offer.features.map(el => {
+    return `
+    <li class="popup__feature popup__feature--${el}"></li>
+    `;
+  }).join('');
+  descPopup.textContent = offer.description;
+  photosPopup.innerHTML = offer.photos.map(el => {
+    return `
+    <img src="${el}" class="popup__photo" width="45" height="40" alt="Фотография жилья"></img>
+    `
+  }).join('');
+  avatarPopup.src = author.avatar;
   cardFragment.appendChild(cardElement);
 });
 
 mapCanvas.appendChild(cardFragment);
-
