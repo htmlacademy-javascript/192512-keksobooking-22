a33eaa103d97cd927b1877c1b7c4b8941f40bcda
Evgeniy Larkov <elarkov@inbox.ru>
2021-03-01 17:02:09 +0700

    Мелкие правки и небольшой рефакторинг

--- js/create-fetch.js
+++ js/create-fetch.js
@@ -16,25 +16,21 @@ const createFetch = (onSuccess, onError) => () => {
     });
 };
 
-// const createSubmit = (dataForm) => {
-//   return fetch('https://22.javascript.pages.academy/keksobooking', {
-//     method: 'POST',
-//     credentials: 'same-origin',
-//     body: dataForm,
-//   })
-//     .then((response) => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//     })
-//     .then((data) => {
-//       onSuccess(data);
-//     })
-//     .catch((err) => {
-//       onError(err);
-//     });
-// };
+const createSubmit = (formData, onSuccess, onError) => {
+  return fetch('https://22.javascript.pages.academy/keksobooking', {
+    method: 'POST',
+    credentials: 'same-origin',
+    body: formData,
+  }).then((response) => {
+    if (response.ok) {
+      onSuccess();
+    }
+  })
+    .catch((err) => {
+      onError(err);
+    });
+};
 
 
 
-export { createFetch };
+export { createFetch, createSubmit };
--- js/validate-form.js
+++ js/validate-form.js
@@ -1,5 +1,6 @@
 import { showMsg } from './show-message.js';
 import { setInitStartPin } from './map.js';
+import { createSubmit } from './create-fetch.js';
 
 const mainForm = document.querySelector('.ad-form');
 
@@ -7,17 +8,16 @@ mainForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const formData = new FormData(evt.target);
 
-  fetch('https://22.javascript.pages.academy/keksobooking', {
-    method: 'POST',
-    credentials: 'same-origin',
-    body: formData,
-  }).then((response) => {
-    if (response.ok) {
+  createSubmit(
+    formData,
+    () => {
       mainForm.reset();
       showMsg();
       setInitStartPin();
-    }
-  });
+    },
+    () => {}
+  );
+
 });
 
 
