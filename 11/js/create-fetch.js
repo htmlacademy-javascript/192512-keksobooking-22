3ea5744b12318a0b51b4b68ec0c94f4be2ae1f12
Keks <keks@htmlacademy.ru>
2021-03-11 17:29:03 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/create-fetch.js
@@ -0,0 +1,32 @@
+const createFetch = (onSuccess) => () => {
+  return fetch('https://22.javascript.pages.academy/keksobooking/data', {
+    method: 'GET',
+    credentials: 'same-origin',
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      onSuccess(data);
+    });
+};
+
+const createSubmit = (formData, onSuccess, onError) => {
+  return fetch('https://22.javascript.pages.academy/keksobooking', {
+    method: 'POST',
+    credentials: 'same-origin',
+    body: formData,
+  }).then((response) => {
+    if (response.ok) {
+      onSuccess();
+    }
+  })
+    .catch((err) => {
+      onError(err);
+    });
+};
+
+
+export { createFetch, createSubmit };
---
+++ 11/js/filter.js
@@ -0,0 +1,90 @@
+import { createMarks, removeMarker } from './map.js';
+import { createFetch } from './create-fetch.js';
+
+const filterForm = document.querySelector('.map__filters');
+const typeHouse = filterForm.querySelector('#housing-type');
+const typePrice = filterForm.querySelector('#housing-price');
+const typeRoom = filterForm.querySelector('#housing-rooms');
+const typeGuest = filterForm.querySelector('#housing-guests');
+
+
+const SIMILAR_OFFER_COUNT = 10;
+const DELAY = 500;
+
+const filterPrice = (offer, price) => {
+  if (price === 'any') {
+    return true;
+  }
+
+  if (offer.offer.price <= 10000 && price === 'low') {
+    return true;
+  }
+  if (offer.offer.price > 10000 && offer.offer.price <= 50000 && price === 'middle') {
+    return true;
+  }
+  if (offer.offer.price > 50000 && price === 'high') {
+    return true;
+  }
+  return false;
+};
+
+const filterTypeHouse = (offer, type) => {
+  if (type === 'any' || offer.offer.type === type) {
+    return true;
+  }
+  return false;
+};
+
+const filterRooms = (offer, room) => {
+  if (room === 'any') {
+    return true;
+  }
+  if (Number(room) === Number(offer.offer.rooms)) {
+    return true;
+  }
+  return false;
+};
+
+const filterGuests = (offer, guest) => {
+  if (guest === 'any') {
+    return true;
+  }
+  if (Number(guest) === offer.offer.guests) {
+    return true;
+  }
+  return false;
+};
+
+const compareFeature = (offer) => {
+  const featureElems = filterForm.querySelectorAll('.map__checkbox:checked');
+  const featureList = Array.from(featureElems).map(el => el.value);
+  return featureList.every((feature) => offer.offer.features.includes(feature));
+};
+
+
+const filterOffers = (offers) => {
+  const elems = offers.filter((offer) => {
+    return filterTypeHouse(offer, typeHouse.value) && filterPrice(offer, typePrice.value) && filterRooms(offer, typeRoom.value) && filterGuests(offer, typeGuest.value) && compareFeature(offer);
+  });
+  return elems;
+};
+
+let offers = [];
+
+
+
+const fetchData = createFetch((data) => {
+  /*global _:readonly*/
+  offers = data;
+  createMarks(offers.slice(0, SIMILAR_OFFER_COUNT));
+  filterForm.addEventListener('change', _.debounce(() => {
+    const filterData = filterOffers(offers);
+    removeMarker();
+    createMarks(filterData.slice(0, SIMILAR_OFFER_COUNT));
+  }, DELAY));
+});
+fetchData();
+
+
+
+
